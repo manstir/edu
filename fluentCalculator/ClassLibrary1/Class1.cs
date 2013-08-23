d09c87b1978d0cea29b80118bd4fef961b10f6ee
manstir <florian@scheiwiller.ch>
2013-08-23 17:23:15 +0200

    Initial Import

---
+++ fluentCalculator/ClassLibrary1/Class1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using NUnit.Framework;
+
+namespace fluentCalculator
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void ExpT()
+        {
+            Expression firstArg = Expression.Constant(2);
+            Expression secondArg = Expression.Constant(3);
+            Expression add = Expression.Add(firstArg, secondArg);
+            Func<int> compiled = Expression.Lambda<Func<int>>(add).Compile();
+            Console.WriteLine(compiled());
+            Console.WriteLine(add);
+        
+            Expression<Func<int>> return5 = () => 5;
+            var cp = return5.Compile();
+            Console.WriteLine(cp());
+
+            Expression<Func<string, string, bool>> expression = (s, s1) => s.StartsWith(s1);
+            var function = expression.Compile();
+            Console.WriteLine(function("foo", "mee"));
+            Console.WriteLine(function("foo mee", "foo"));
+
+            
+        }
+
+        [Test]
+        public void Sample1()
+        {
+            Assert.AreEqual(20, (int)new Calculator()
+                .Calc(10)
+                .Plus(5)
+                .Minus(2)
+                .Undo()
+                .Redo()
+                .Undo()
+                .Plus(5));
+        }
+
+        [Test]
+        public void Sample2()
+        {
+            Assert.AreEqual(13, (int)new Calculator()
+                .Calc(10)
+                .Plus(5)
+                .Minus(2)
+                .Undo()
+                .Undo()
+                .Redo()
+                .Redo()
+                .Redo());
+        }
+
+        [Test]
+        public void Sample3()
+        {
+            Assert.AreEqual(18, (int)new Calculator()
+                .Calc(10)
+                .Plus(5)
+                .Minus(2)
+                .Save()
+                .Undo()
+                .Redo()
+                .Undo()
+                .Plus(5));
+        }
+    }
+
+    public class Calculator
+    {
+        private Stack<Func<int, int>> _actions; 
+        private Stack<Func<int, int>> _undone; 
+
+        public Calculator Calc(int n)
+        {
+            Init(n);
+            return this;
+        }
+
+        private void Init(int n)
+        {
+            ResetActionList();
+            Plus(n);
+            ResetUndoList();
+        }
+
+        public Calculator Plus(int n)
+        {
+            _actions.Push((a) => a + n);
+            return this;
+        }
+
+        public Calculator Minus(int n)
+        {
+            _actions.Push((a) => a - n);
+            return this;
+        }
+
+        public Calculator Undo()
+        {
+            if (_actions.Count > 1) // cannot undo first init action!
+            {
+                _undone.Push(_actions.Pop());
+
+            }
+            return this;
+        }
+
+        public Calculator Redo()
+        {
+            if (_undone.Any())
+            {
+                _actions.Push(_undone.Pop());
+            }
+            return this;
+        }
+
+        public Calculator Save()
+        {
+            Init(Result());
+            return this;
+        }
+
+        private int Result()
+        {
+            return _actions.Aggregate(0, (current, f) => f(current));
+        }
+
+        public static explicit operator int(Calculator c)
+        {
+            return c.Result();
+        }
+
+        private void ResetActionList()
+        {
+            _actions = new Stack<Func<int, int>>();
+        }
+
+        private void ResetUndoList()
+        {
+            _undone = new Stack<Func<int, int>>();
+        }
+    }
+}
---
+++ fluentCalculator/fluentCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace fluentCalculator
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void test()
+        {
+                Assert.AreEqual(10, (int)new Calculator().Calc(10));
+        }
+    }
+
+    public class Calculator
+    {
+        private Calculator _instance;
+        private int _startInt;
+
+        public Calculator Calc(int n)
+        {
+            _startInt = n;
+            return this;
+        }
+
+        // change explicit to implicit depending on what you need
+        public static explicit operator int(Calculator c)
+        {
+            return c._startInt;
+        }
+    }
+
+
+}
